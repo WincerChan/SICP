d1d9e8cb0466c4e5906e53acbd8e922295204c9e
WincerChan <WincerChan@jxnu.edu.cn>
2017-07-23 21:38:50 +0800

    rebuild code and fixed some bugs

--- chapter1/e1.28.scm
+++ chapter1/e1.28.scm
@@ -1,22 +1,23 @@
-(define (check-prime n)
-  (if (= n 2) #t
-      (Miller-test 2 n)))
-
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
+        ;; ((= (remainder (square base) m) 1) 0)
+        ((nontrivial-square-root? base m) 0)
         ((even? exp)
          (remainder (square (expmod base (/ exp 2) m))
                     m))
-        ((= (remainder (square base) m) 1)0)
         (else
          (remainder (* base (expmod base (- exp 1) m))
                     m))))
 
-(define (Miller-test a n)
-  (cond ((= a (- n 2)) #t)
-        ((= (expmod a (- n 1) n) 1)
-         (Miller-test (+ a 1) n))
-        (else #f)))
+(define (miller-rabin n)
+  (miller-rabin-test (- n 1) n))
 
-(define (even? n)
-  (= (remainder n 2) 0))
+(define (miller-rabin-test a n)
+  (cond ((= a 0) true)
+        ((= (expmod a (- n 1) n) 1) (miller-rabin-test (- a 1) n))
+        (else false)))
+
+(define (nontrivial-square-root? a n)
+  (and (not (= a 1))
+       (not (= a (- n 1)))
+       (= 1 (remainder (square a) n))))
