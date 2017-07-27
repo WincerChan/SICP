03cc8b1b2f9d23a7ee454b944bcc436fcc9cc81a
WincerChan <WincerChan@jxnu.edu.cn>
2017-07-27 12:03:45 +0800

    use compose to refactor code

--- chapter1/e1.43.scm
+++ chapter1/e1.43.scm
@@ -1,14 +1,7 @@
-;; 迭代
-(define (repeat-iter f n result)
-  (if (= n 0)
-      result
-      (repeat-iter f (- n 1) (f result))))
-
-;; 递归
-(define (repeat-iter f n x)
-  (if (= n 0)
-      x
-      (f (repeat-iter f (- n 1) x))))
-
 (define (repeated f n)
-  (lambda (x) (repeat-iter f n x)))
+  (if (= n 1)
+      f
+      (compose f
+               (repeated f (- n 1)))))
+(define (compose f g)
+  (lambda (x) (f (g x))))
