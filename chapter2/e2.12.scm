ddadadf6bba6e590244796c1dbbfd181e16399fc
WincerChan <WincerChan@jxnu.edu.cn>
2017-08-04 10:59:33 +0800

    Add Exercise 2.12

---
+++ chapter2/e2.12.scm
@@ -0,0 +1,12 @@
+;; (define (make-center-width c w)
+;;   (make-interval (- c w) (+ c w)))
+
+;; (define (center i)
+;;   (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+;; (define (width i)
+;;   (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (let ((percent c p) (* c p))
+    (make-interval (- c (percent c p)) (+ c (percent c p)))))
