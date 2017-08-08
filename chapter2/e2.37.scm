55fafb4730dd21b3683c78983492952b181b78ca
WincerChan <WincerChan@jxnu.edu.cn>
2017-08-08 12:04:47 +0800

    Add Exercise 2.37

---
+++ chapter2/e2.37.scm
@@ -0,0 +1,12 @@
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (a) (dot-product a v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
