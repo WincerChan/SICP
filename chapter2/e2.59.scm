0b7f049ac395672bad2203e5c7445d12919ea3f1
WincerChan <WincerChan@jxnu.edu.cn>
2017-08-12 17:08:24 +0800

    Add Exercise 2.59

---
+++ chapter2/e2.59.scm
@@ -0,0 +1,13 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (union-set set1 set2)
+  (define (union-iter set3)
+    (cond ((or (null? set1) (null? set2)) '())
+          ((element-of-set? (car set1) set2)
+           (cons (car set1)
+                 (union-set (cdr set1) set2)))
+          (else (union-set (cdr set1) set2))))
+  (union-iter '()))
