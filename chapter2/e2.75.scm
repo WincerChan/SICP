bf121995d99f814123b1ec8bd75513609576ca31
WincerChan <WincerChan@jxnu.edu.cn>
2017-08-16 15:26:24 +0800

    Add Exercise 2.75

---
+++ chapter2/e2.75.scm
@@ -0,0 +1,9 @@
+(define (make-from-real-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else
+           (error "Unknow op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
